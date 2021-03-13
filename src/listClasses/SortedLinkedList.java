5c8ca9dd5a3c902906f7f0076ae7bef37fbd73e5
Abdullah-yasir <56272322+Abdullah-yasir@users.noreply.github.com>
2021-03-13 13:27:08 +0500

    fixed add method in sorted list, now its adds and sorts

--- src/listClasses/SortedLinkedList.java
+++ src/listClasses/SortedLinkedList.java
@@ -12,57 +12,51 @@ import java.util.*;
  */
 
 public class SortedLinkedList<T> extends BasicLinkedList<T> {
-	private Comparator<T> comparator ;
+	private Comparator<String> comparator ;
 
-	public SortedLinkedList(Comparator<T> c){
+	public SortedLinkedList(Comparator<String> c){
 		this.comparator = c;
 	}
 
-	protected class SortListComp implements Comparator<T>{
-		@Override
-		public int compare(T o1, T o2) {
-			if(o1.toString().compareTo(o2.toString()) == 0){
-				return 1;
-			}
-			return -1;
+	class SortListComparator implements Comparator<String>{
+		public int compare(String s1, String s2) {
+			return s1.length() - s2.length();
 		}
 	}
+
 	public SortedLinkedList<T> add(T data){
-		// throw new UnsupportedOperationException("This method is not implemented yet!");
-		Node nodeToAdd = new Node(data);
-		Node current = head, index = null;
-		T temp;
-		
-		// comparator.compare(head, nodeToAdd);
-        // if head is null, assign incoming node to it
-        if(current == null){
-            head = tail = nodeToAdd;
-            return this;
-        }
-
-        // keep looping until we find a node with reference to null in its 'next'
-        while(current.next != null){
-            current = current.next;
-        }
-        // append nodeToAdd to the last node in list
-		current.next = nodeToAdd;
-		tail = nodeToAdd;
-		listSize++;
-
-		// sort the list 
-		index = current.next;
-		while(current.next != null){
-			if(current.data.toString().compareTo(index.data.toString()) == 0){
-				temp = current.data;
-				current.data = index.data;
-				index.data = temp;
-			}
-			if(index.next != null) index = index.next;
-			else break;
+		if (data == null) {
+			return this;
 		}
+
+		Node newnode = new Node(data);
+		if (head == null) {
+		// The list was empty
+		head = tail = new Node(data);
+		} else {
+			// Check if it needs to go right at the head
+			if (comparator.compare(data.toString(), head.data.toString()) <= 0) {
+				newnode.next = head;
+				head = newnode;
+			}
+			// Check if it needs to go right at the tail
+			else if (comparator.compare(data.toString(), tail.data.toString()) >= 0) {
+				tail.next = newnode;
+				tail = newnode;
+			} else {
+				// It needs to be inserted into the middle of the list
+				Node next = head.next;
+				Node prev = head;
+				while (comparator.compare(data.toString(), next.data.toString()) > 0) {
+				prev = next;
+				next = next.next;
+				}
+				// Do the actual insertion
+				prev.next = newnode;
+				newnode.next = next;
+			}
+		}
+		listSize++;
 		return this;
-		
-	}
-
-
+  	}
 }
